a1544a434990c53e205738d6760f8dc4ce151643
ahmetbcakici <ahmetbcakici@gmail.com>
2020-08-12 03:51:04 +0300

    get folder names for repo opening page

---
+++ api/src/index.ts
@@ -0,0 +1,49 @@
+import fastify, {FastifyInstance} from 'fastify';
+import cheerio from 'cheerio';
+import request from 'request';
+
+const server: FastifyInstance = fastify();
+
+server.get('/ping', async (req, reply) => {
+  /* NOT: 2 normalde başlangıçtır ama sub folderlarda 3 başlangıçtır zira 2 (...) üste çıkış yeridir */
+  /* console.log(siteBody.find('.commit-author').text()) */
+
+  request('https://github.com/ahmetbcakici/fullstack-slido-clone', function (
+    error,
+    response,
+    body
+  ) {
+    const $ = cheerio.load(body);
+    const siteBody = $('body');
+    let i = 2;
+    while (true) {
+      let y = siteBody
+        .find(
+          `div.Box-row:nth-child(${i}) > div:nth-child(2) > span:nth-child(1)`
+        )
+        .text();
+      if (!y) break;
+
+      console.log(y);
+      i++;
+    }
+    /* return console.log(
+      siteBody
+        .find('div.Box-row:nth-child(3) > div:nth-child(2) > span:nth-child(1)')
+        .text()
+    ); */
+
+    /* console.log(siteBody.find('div.Details-content--hidden-not-important:nth-child(1)').text()) */
+
+    /* div.Box-row:nth-child(2) > div:nth-child(2) > span:nth-child(1) */
+    /* div.Box-row:nth-child(3) > div:nth-child(2) > span:nth-child(1) */
+  });
+});
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err);
+    //process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
