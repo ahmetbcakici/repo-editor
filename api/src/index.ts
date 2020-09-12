6292209b1febf97c7f31e95b185e4e43ec2ded2d
ahmetbcakici <ahmetbcakici@gmail.com>
2020-09-12 23:39:25 +0300

    use github api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,50 +1,49 @@
-import fastify, {FastifyInstance} from 'fastify';
-import cheerio from 'cheerio';
-import request from 'request';
+import fastify, { FastifyInstance } from 'fastify';
+import axios from 'axios'
+
+/* type item = {
+  path: string,
+  mode: string,
+  type: string,
+  sha: string,
+  size: number,
+  url: string
+} */
 
 const server: FastifyInstance = fastify();
 
+let obj: object[];
+(async function(){
+  const { data: { tree } } = await axios.get('https://api.github.com/repos/ahmetbcakici/repo-editor/git/trees/44c3b1318cda3739a114ae92292e97b36aa98a5a')
+  tree.map((item: any) => {
+    console.log(item) 
+    obj.push({})
+    if (item.type === 'tree') console.log(item.path)
+  })
+})()
+
 server.get('/ping', async (req, reply) => {
-  /* NOT: 2 normalde başlangıçtır ama sub folderlarda 3 başlangıçtır zira 2 (...) üste çıkış yeridir */
-  /* console.log(siteBody.find('.commit-author').text()) */
-
-  request('https://github.com/ahmetbcakici/fullstack-slido-clone', function (
-    error,
-    response,
-    body
-  ) {
-    const $ = cheerio.load(body);
-    const siteBody = $('body');
-    let i = 2;
-    while (true) {
-      let item = siteBody
-        .find(
-          `div.Box-row:nth-child(${i}) > div:nth-child(2) > span:nth-child(1)`
-        )
-        .text();
-        
-      let isFolder = siteBody
-        .find(
-          `div.Box-row:nth-child(${i}) > div:nth-child(1) > svg:nth-child(1)`
-        )
-        .hasClass('octicon-file-directory');
-      if (!item) break;
-
-      console.log(isFolder, item);
-      i++;
-    }
-    /* return console.log(
-      siteBody
-        .find('div.Box-row:nth-child(3) > div:nth-child(2) > span:nth-child(1)')
-        .text()
-    ); */
-
-    /* console.log(siteBody.find('div.Details-content--hidden-not-important:nth-child(1)').text()) */
-
-    /* div.Box-row:nth-child(2) > div:nth-child(2) > span:nth-child(1) */
-    /* div.Box-row:nth-child(3) > div:nth-child(2) > span:nth-child(1) */
-  });
+  reply.send('pong')
 });
+
+server.get('/', async (req, reply) => {
+  const { data: { tree } } = await axios.get('https://api.github.com/repos/ahmetbcakici/repo-editor/git/trees/44c3b1318cda3739a114ae92292e97b36aa98a5a')
+  tree.map((item: any) => {
+    console.log(item) 
+    if (item.type === 'tree') console.log(item.path)
+  })
+});
+/* 
+server.get('/x', async (req, reply) => {
+  const tree = [{}, {}]
+  tree.map((item: any) => {
+    if (item.type === 'blob'){
+      const itemData = item.url;
+      const content = itemData.content;
+      const decodedContent = atob(content)
+    }
+  })
+}); */
 
 server.listen(8080, (err, address) => {
   if (err) {
